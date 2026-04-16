9e2fc435ef3916b12e9755dcaf9d92a1ec88bd5f
baer1 <videocreator@outlook.de>
2026-04-16 19:20:15 +0200

    Add pre- and post build

--- build.sh
+++ build.sh
@@ -11,9 +11,18 @@ if [ -f build.sh ]; then
 fi
 
 # Default build script (fallback)
-echo -e "\e[1;94m==== BUILD ====\e[0m"
+
+if [ -f prebuild.sh ]; then
+  echo -e "\e[1;94m==== PRE-BUILD ====\e[0m"
+  bash prebuild.sh
+fi
+echo -e "\e[1;94m====   BUILD   ====\e[0m"
 dotnet build -c Release -o build
-echo -e "\e[1;94m====  ZIP  ====\e[0m"
+echo -e "\e[1;94m====    ZIP    ====\e[0m"
 zip "release.zip" -jMM "manifest.json" build/**.dll
 zip "release.zip" -j "icon.png" "README.md"
-echo -e "\e[1;94m===============\nRelease file created at \e[32m\"release.zip\"\e[0m"
+if [ -f postbuild.sh ]; then
+  echo -e "\e[1;94m==== POST-BUILD ====\e[0m" # I hate this one character offset so much
+  bash postbuild.sh
+fi
+echo -e "\e[1;94m====================\nRelease file created at \e[32m\"release.zip\"\e[0m"
