d9beadc4da9e5381dda69107a6806ae710ad1297
baer1 <videocreator@outlook.de>
2026-04-16 18:19:28 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s globstar
+
+cd "$(dirname "$0")"
+cd target
+
+# Check for user build script (should create release.zip in working directory)
+if [ -f build.sh ]; then
+  exec bash build.sh
+fi
+
+# Default build script (fallback)
+echo -e "\e[1;94m==== BUILD ====\e[0m"
+dotnet build -c Release -o build
+echo -e "\e[1;94m====  ZIP  ====\e[0m"
+zip "release.zip" -jMM "manifest.json" build/**.dll
+zip "release.zip" -j "icon.png" "README.md"
+echo -e "\e[1;94m===============\nRelease file created at \e[32m\"release.zip\"\e[0m"
